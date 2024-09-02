380f08cd125badc7e49b30a949fbf8b5927c22f5
VanyaKpop <165906351+VanyaKpop@users.noreply.github.com>
2024-09-02 17:56:11 +0600

    Update

---
+++ ArrayExtensions/ArrayExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Shedule.ArrayExtensions
+{
+	static public class ArrayExtensions
+	{
+
+		static public string IsEmpty(this string SomeThing)
+		{
+			if (SomeThing == null || SomeThing == " ")
+			{
+				return "Пусто";
+			}
+
+			return SomeThing;
+		}
+	}
+}
